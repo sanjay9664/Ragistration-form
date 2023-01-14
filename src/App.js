b45049187f998431782e1d1e4888bdb1eaf1b0a0
sanjay gupta <guptaji30749@gmail.com>
2023-01-14 09:36:15 +0530

    ragistration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';  // am already write the css 
+
+// import Formdata from "./component/Formdata"
+// import Data from "./component/Data"
+import Form from "./component/Form"    // go to the live server or browser
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+  
+   {/* <Formdata/> */}
+   {/* <Data/> */}
+
+   <Form/>
     </div>
   );
 }
